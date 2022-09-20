8c004905c178ae6f33a7e25c87396bcd9b2b8297
Long9725 <develop9725@gmail.com>
2022-09-20 15:09:10 +0900

    feat: upload nft layer image
    Upload nft layer image to path, 'layers/{collection}/{layer}
    /{item}#{percetange}.png'. So image must be png.
    
    feature

--- routers/main.js
+++ routers/main.js
@@ -1,18 +1,82 @@
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+
+const express = require("express");
+const app = express();
+
+const multer = require("multer");
+
 const basePath = process.cwd();
 const { startCreating, buildSetup } = require(`${basePath}/src/main.js`);
 
-module.exports = function(app)
-{
-     app.get('/',function(req,res){
-        console.log('hello world')
-     });
+const handleError = (err, res) => {
+    res
+        .status(500)
+        .contentType("text/plain")
+        .end("Oops! Something went wrong!");
+};
 
-     app.get('/create-new-collection', function(req, res){
+const upload = multer({
+    dest: "../layers"
+    // you might also want to set some limits: https://github.com/expressjs/multer#limits
+});
+
+const mkdirLayersIfNotExist = (collection, property) => {  
+    layersDir = `${basePath}/layers/${collection}`;
+    layersPropertyDir = `${layersDir}/${property}`
+    if (!fs.existsSync(layersDir)) {
+        fs.mkdirSync(layersDir);
+    }
+
+    if (!fs.existsSync(layersPropertyDir)) {
+        fs.mkdirSync(`${layersPropertyDir}`);
+    }
+  };
+
+module.exports = function (app) {
+    app.get("/", express.static(path.join(__dirname, "../public")));
+    app.post(
+        "/upload-layer",
+        upload.single("file" /* name attribute of <file> element in your form */),
+        (req, res) => {
+            const tempPath = req.file.path;
+            const targetPath = path.join(__dirname, `../layers/${req.body.collection}/${req.body.property}/${req.body.item}#${req.body.percentage}.png`);
+
+            mkdirLayersIfNotExist(req.body.collection, req.body.property)
+
+            if (path.extname(req.file.originalname).toLowerCase() === ".png") {
+                fs.rename(tempPath, targetPath, err => {
+                    if (err) return handleError(err, res);
+
+                    res
+                        .status(200)
+                        .contentType("text/plain")
+                        .end("File uploaded!");
+                });
+            } else {
+                fs.unlink(tempPath, err => {
+                    if (err) return handleError(err, res);
+
+                    res
+                        .status(403)
+                        .contentType("text/plain")
+                        .end("Only .png files are allowed!");
+                });
+            }
+        }
+    );
+    app.get('/create-new-collection', function (req, res) {
         const namePrefix = req.query.name;
         const description = req.query.description;
         buildSetup(namePrefix);
+        
+        // TODO: layersConfiguration modify
         startCreating(namePrefix, description);
-        result = {"success": 1};
+        result = { "success": 1 };
         res.json(result)
-     })
+    })
 }
+
+
+
